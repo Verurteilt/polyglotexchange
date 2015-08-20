3714a625a23285d6c8bdd968f825d1a599e494bf
victor <mini.guero@hotmail.com>
2015-08-20 02:52:20 -0500

    Starting Q&A

--- polyglotexchange/qa/models.py
+++ polyglotexchange/qa/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Fav(models.Model):
+	question = models.ForeignKey(Question)
+	user = models.ForeignKey('users.User')
+
+
+
+
+
+class Question(models.Model):
+	question = models.CharField(max_length=250)
+	body = models.TextField(max_length=250)
+	owner = models.ForeignKey('users.User')
+	upvotes = models.IntegerField()
+	downvotes = models.IntegerField()
+	created = models.DateTimeField(auto_now_add=True)
+
